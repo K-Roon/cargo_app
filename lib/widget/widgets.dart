c270311d2c9a7d43b1ba8dbc188b5ebd18b65d66
K-Roon <66551757+K-Roon@users.noreply.github.com>
2021-05-18 16:52:56 +0900

    로그인 레이아웃 완성

--- lib/widget/widgets.dart
+++ lib/widget/widgets.dart
@@ -6,28 +6,33 @@ InputDecoration textFieldInputDecoration(String hintText) {
       hintText: hintText,
       hintStyle: TextStyle(color: Colors.black26),
       focusColor: Color(0xFF0073FF),
+      contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
       focusedBorder: OutlineInputBorder(
         borderSide: BorderSide(color: Color(0xFF0073FF)),
-        borderRadius: BorderRadius.circular(13),
+        borderRadius: BorderRadius.circular(10),
       ),
       enabledBorder: OutlineInputBorder(
         borderSide: BorderSide(color: Colors.black26),
-        borderRadius: BorderRadius.circular(13),
+        borderRadius: BorderRadius.circular(10),
       ));
 }
 
 TextStyle simpleTextStyle() {
-  return TextStyle(color: Colors.white, fontSize: 16);
+  return TextStyle(color: Colors.black, fontSize: 16);
+}
+
+TextStyle buttonTextStyle() {
+  return TextStyle(color: Colors.black, fontSize: 16);
 }
 
 TextStyle biggerTextStyle() {
-  return TextStyle(color: Colors.white, fontSize: 18);
+  return TextStyle(color: Colors.black, fontSize: 20);
 }
 
 TextStyle mediumTextStyle() {
-  return TextStyle(color: Colors.white, fontSize: 15);
+  return TextStyle(color: Colors.black, fontSize: 15);
 }
 
 TextStyle smallTextStyle() {
-  return TextStyle(color: Colors.white, fontSize: 10);
+  return TextStyle(color: Colors.black, fontSize: 10);
 }
