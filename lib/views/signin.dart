2291b4d5ca87a4c44b24699124d9588c9d00dcc1
K-Roon <66551757+K-Roon@users.noreply.github.com>
2021-05-18 13:12:17 +0900

    ADD:아이디 및 비밀번호 필드 추가

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -1,4 +1,6 @@
+import 'package:cargo_app/widget/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SignIn extends StatefulWidget {
   SignIn();
@@ -12,27 +14,65 @@ TextEditingController passwordTextEditingController =
     new TextEditingController();
 
 class _SignInState extends State<SignIn> {
-  final formKey = GlobalKey<FormState>();
+  final signinFormKey = GlobalKey<FormState>();
   bool isLoading = false;
 
   signIn() async {
-    if (formKey.currentState.validate()) {
-
-    }
+    if (signinFormKey.currentState.validate()) {}
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(elevation: 0.0),
+      appBar: AppBar(
+        elevation: 0.0,
+        toolbarHeight: 0.0,
+      ),
       //resizeToAvoidBottomInset: false,
       body: isLoading
           ? Container(
               child: Center(child: CircularProgressIndicator()),
             )
           : Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Form(
+                    key: signinFormKey,
+                    child: Column(
+                      children: [
+                        ///화물선생 로고
+                        Center(
+                          child: Column(
+                            children: [
+                              Text("스마트한 화물 배차의 시작"),
+                              Text(
+                                "화물선생",
+                                style: TextStyle(
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFF0073FF)),
+                              )
+                            ],
+                          ),
+                        ),
 
-      ),
+                        ///아이디 및 비밀번호 입력 창
+                        TextFormField(
+                          style: formTextStyle(),
+                          decoration: textFieldInputDecoration("아이디"),
+
+                        ),TextFormField(
+                          style: formTextStyle(),
+                          decoration: textFieldInputDecoration("비밀번호"),
+
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
     );
   }
 }
