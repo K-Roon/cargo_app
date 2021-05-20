d5df7f88fb70eb8f2e3d12ba5949058bc09ed0bf
K-Roon <66551757+K-Roon@users.noreply.github.com>
2021-05-20 12:02:57 +0900

    FEAT : 색상 수정 및 테마 수정
    author : 권재현

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -1,6 +1,7 @@
 import 'package:cargo_app/widget/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class SignIn extends StatefulWidget {
   SignIn();
@@ -9,16 +10,70 @@ class SignIn extends StatefulWidget {
   _SignInState createState() => _SignInState();
 }
 
-TextEditingController emailTextEditingController = new TextEditingController();
+TextEditingController idTextEditingController = new TextEditingController();
 TextEditingController passwordTextEditingController =
     new TextEditingController();
 
 class _SignInState extends State<SignIn> {
   final signinFormKey = GlobalKey<FormState>();
   bool isLoading = false;
+  bool isAvailable = false;
 
   signIn() async {
     if (signinFormKey.currentState.validate()) {}
+  }
+
+  void showErrorAlertDialog(BuildContext context, String errorText) async {
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            elevation: 0.0,
+            titlePadding: EdgeInsets.zero,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            title: Container(
+                decoration: BoxDecoration(
+                    color: Color(0xff0055ff),
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(10))),
+                padding: EdgeInsets.all(10),
+                alignment: Alignment.center,
+                child: Text(
+                  "안내",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                )),
+            contentPadding: EdgeInsets.only(top: 10),
+            content: Text(
+              errorText,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black, fontSize: 15),
+            ),
+            actionsPadding: EdgeInsets.all(10),
+            actions: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    decoration: BoxDecoration(
+                        color: Color(0xff0055ff),
+                        borderRadius: BorderRadius.circular(3)),
+                    child: Text(
+                      "확인",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    )),
+              )
+            ],
+          );
+        });
   }
 
   @override
@@ -37,7 +92,7 @@ class _SignInState extends State<SignIn> {
               padding: EdgeInsets.symmetric(horizontal: 20),
               child: Column(
                 children: [
-                  Container(height: 20),
+                  Container(height: 50),
 
                   ///화물선생 로고
                   Center(
@@ -55,7 +110,7 @@ class _SignInState extends State<SignIn> {
                     ),
                   ),
 
-                  Container(height: 30),
+                  Container(height: 50),
 
                   ///아이디 및 비밀번호 입력 창(폼)
                   Form(
@@ -64,17 +119,33 @@ class _SignInState extends State<SignIn> {
                       children: [
                         TextFormField(
                           decoration: textFieldInputDecoration("아이디"),
+                          validator: (val) {
+                            if (val.isEmpty) {
+                              return "아이디를 입력해주세요.";
+                            } else {
+                              return null;
+                            }
+                          },
                           style:
                               TextStyle(color: Color(0xFF0073FF), fontSize: 16),
                           textInputAction: TextInputAction.next,
+                          controller: idTextEditingController,
                         ),
                         Container(height: 15),
                         TextFormField(
                           obscureText: true,
+                          validator: (val) {
+                            if (val.isEmpty) {
+                              return "비밀번호를 입력해주세요.";
+                            } else {
+                              return null;
+                            }
+                          },
                           style:
                               TextStyle(color: Color(0xFF0073FF), fontSize: 16),
                           decoration: textFieldInputDecoration("비밀번호"),
                           textInputAction: TextInputAction.go,
+                          controller: passwordTextEditingController,
                         ),
                       ],
                     ),
@@ -160,10 +231,27 @@ class _SignInState extends State<SignIn> {
                             )
                           ],
                         ),
-                      ))
+                      )),
                 ],
               ),
             ),
+      bottomNavigationBar: GestureDetector(
+        onTap: () {
+          if (isAvailable) {
+            print("로그인 버튼 누름");
+          } else {
+            showErrorAlertDialog(context, "아이디나 비밀번호를 써주세요.");
+          }
+        },
+        child: Container(
+            height: 80,
+            color: isAvailable ? Color(0xff0055ff) : Color(0xff8d9699),
+            alignment: Alignment.center,
+            child: Text(
+              "로그인",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            )),
+      ),
     );
   }
 }
