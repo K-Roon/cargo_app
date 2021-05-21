926631ff84ee9776fac3b8c8cb38b93b7fb2c93e
K-Roon <66551757+K-Roon@users.noreply.github.com>
2021-05-21 16:27:14 +0900

    Revert "Revert "FEAT:비밀번호 입력창 추가 및 홈화면 추가""
    This reverts commit ba898e70

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 //import 'package:chat_app/views/chat_rooms_screen.dart';
 //import 'package:firebase_core/firebase_core.dart';
 import 'package:cargo_app/views/signin.dart';
+import 'package:cargo_app/views/signup/signup_step3.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
