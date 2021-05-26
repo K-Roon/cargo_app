cb3a91f279e9c1cc006879e22caae413afd9d6b9
K-Roon <66551757+K-Roon@users.noreply.github.com>
2021-05-26 13:53:39 +0900

    FEAT : 주소 검색기능 추가 및 버그 개선

--- lib/views/home_departure.dart
+++ lib/views/home_departure.dart
@@ -4,6 +4,7 @@ import 'package:cargo_app/widget/margin_bar.dart';
 import 'package:cargo_app/widget/textInputDeco.dart';
 import 'package:cargo_app/widget/widgets.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
@@ -27,6 +28,10 @@ class _Home_departureState extends State<Home_departure> {
   bool isLoading = false;
   bool isAvailable = false;
   int point = 0;
+  final double _initFabHeight = 120.0;
+  double _fabHeight = 0;
+  double _panelHeightOpen = 0;
+  double _panelHeightClosed = 95.0;
 
   final List<Tab> myTabs = <Tab>[
     Tab(text: '최근 주소'),
@@ -37,6 +42,7 @@ class _Home_departureState extends State<Home_departure> {
 
   @override
   Widget build(BuildContext context) {
+    _panelHeightOpen = MediaQuery.of(context).size.height * .70;
     return Scaffold(
       resizeToAvoidBottomInset: false,
       key: _ScaffoldState,
@@ -103,10 +109,11 @@ class _Home_departureState extends State<Home_departure> {
       borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
       defaultPanelState: PanelState.OPEN,
       minHeight: 25,
-      maxHeight: 500,
+      maxHeight: _panelHeightOpen,
       panel: Container(
-        margin: EdgeInsets.symmetric(horizontal: 20),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             ///회색계열의 Handle
@@ -118,7 +125,50 @@ class _Home_departureState extends State<Home_departure> {
                   color: Colors.black54,
                   borderRadius: BorderRadius.all(Radius.circular(12.0))),
             ),
-            isDeparture ? pg1() : pg2()
+            Container(
+              height: 30,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: Form(
+                child: Material(
+                  elevation: 20,
+                  shadowColor: Colors.black54,
+                  borderRadius: BorderRadius.circular(10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: isDeparture ? "출발지 주소 검색" : "도착지 주소 검색",
+                        prefixIcon: Icon(Icons.search,
+                            size: 20, color: Colors.blueAccent),
+                        hintStyle: TextStyle(color: Colors.black26),
+                        fillColor: Colors.white,
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: 0.0, horizontal: 20.0),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black26, width: 0.1),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black26, width: 0.1),
+                          borderRadius: BorderRadius.circular(10),
+                        )),
+                  ),
+                ),
+              ),
+            ),
+            DefaultTabController(
+              length: myTabs.length,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TabBar(tabs: myTabs),
+
+                ],
+              ),
+            )
           ],
         ),
       ),
@@ -126,51 +176,30 @@ class _Home_departureState extends State<Home_departure> {
   }
 
   Widget pg1() {
-    return Column(
-      children: [
-        Container(
-          height: 30,
-        ),
-        Form(
-          child: Material(
-            elevation: 20,
-            shadowColor: Colors.black54,
-            borderRadius: BorderRadius.circular(10),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  hintText: "출발지 주소 검색",
-                  prefixIcon: Icon(Icons.search,
-                      size: 20, color: Colors.blueAccent),
-                  hintStyle: TextStyle(color: Colors.black26),
-                  fillColor: Colors.white,
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Colors.black26, width: 0.1),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  border: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Colors.black26, width: 0.1),
-                    borderRadius: BorderRadius.circular(10),
-                  )),
-            ),
-          ),
-        ),
-        DefaultTabController(
-          length: myTabs.length,
-          child: Column(children: [
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
             TabBar(tabs: myTabs),
             TabBarView(
-              children: myTabs.map((Tab tab) {
-                final String label = tab.text;
-                return Center(child: Text(label));
-              }).toList(growable: false),
-            ),
+                dragStartBehavior: DragStartBehavior.start,
+                children: <Widget>[
+                  new Center(
+                    child: Text("무야호"),
+                  ),
+                  new Text(
+                    " TabBarView placeholder 2",
+                    style: new TextStyle(
+                        fontSize: 12.0,
+                        color: const Color(0xFF000000),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"),
+                  )
+                ]),
           ]),
-        ),
-      ],
     );
   }
 
